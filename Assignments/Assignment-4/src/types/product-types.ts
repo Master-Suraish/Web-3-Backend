10b93fa7b0043ecfd57ce066f5b3c4f1bb00eea6
Suraish Kirshan <sureshkamar3000@gmail.com>
2025-12-25 15:48:47 +0500

    Delete Assignments/Assignment-4 directory

--- Assignments/Assignment-4/src/types/product-types.ts
+++
@@ -1,9 +0,0 @@
-import { Document } from "mongoose";
-
-export interface ProductTypes extends Document {
-  title: string;
-  description: string;
-  price: number;
-  quantity: number;
-  category: string;
-}
